273c74f30e3574050660390659b6bca440b68e83
RahulMandyal1 <93306066+RahulMandyal1@users.noreply.github.com>
2022-01-24 18:18:35 +0530

    updated Assignment

--- block-BJaaet/code/script.js
+++ block-BJaaet/code/script.js
@@ -1,19 +1,63 @@
-let movieBox  = document.querySelector('form');
-// let movieInput = document.querySelector('moviename');
+let movieBox  = document.querySelector('#moviename');
+let  root = document.querySelector('ul');
+let userInput  = document.querySelector('.moviename');
 
-function handleEvent(event){
-    event.preventDefault();
-    let movieList = document.createElement('div');
-    movieList.classList.add('movielist');       
-    let checkbox = document.createElement(`input`);
-    checkbox.setAttribute("type" , "checkbox");
-    console.log(checkbox);
-    checkbox.checked = false;
-    movieList.append(checkbox);
-    let addMovieName = document.createElement('label');
-    movieList.append(addMovieName);
-    addMovieName.prepend(checkbox);
-    addMovieName.innerHTML = event.target.moviename.value;
-    movieBox.append(addMovieName);
+let movies  = [{
+    name : "titanic",
+    watched : false 
+},
+{
+    name : "fast and furious",
+    watched : true
 }
-movieBox.addEventListener('submit',handleEvent);
+];
+let movieInput = document.querySelector('moviename');
+function deleteMovie(event){
+    let  id  = event.target.dataset.id;
+    movies.splice(id , 1);
+    createMovieUi();
+}
+
+
+
+    function createMovieUi (){
+        root.innerHTML = "";
+        movies.forEach((ele , index)=>{
+            let li = document.createElement('li');
+            let  input  = document.createElement('input');
+            let span   = document.createElement('span');
+            span.innerHTML = "❌";
+            span.setAttribute('data-id',index);
+
+
+            let label= document.createElement('label');
+            label.innerHTML = ele.name;
+            input.type= "checkbox";
+            input.checked = ele.watched;
+            input.id = index;
+            li.append(input,label,span);
+            root.append(li);
+
+
+            span.addEventListener('click',deleteMovie)
+        });
+    }
+      
+
+
+    
+   
+
+movieBox.addEventListener('keyup',function (event){
+    console.log(event.keyCode);
+    if(event.keyCode===13){
+        console.log(event.target);
+        movies.push({
+            name : event.target.value,
+            watched : false
+        });
+          
+    createMovieUi();
+    }
+}
+);
